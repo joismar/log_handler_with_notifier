d7e000c4d8e17ebde06828b957e70d31b0fde39b
Joismar Braga <joismar@outlook.com>
2022-09-28 20:47:32 -0300

    Update and rename custom_logger_with_discord_notifier.py to logger_with_discord_notifier.py

--- logger_with_discord_notifier.py
+++ logger_with_discord_notifier.py
@@ -1,12 +1,16 @@
 from logging import ERROR
+from .logger_with_notifier import NotifierHandler
 from .discord_notifier import DiscordNotifier
 from .logger import Logger
 
-class ErrorLoggerNotifier(Logger):
+
+class LoggerWithDiscordNotifier(Logger):
+    
     def __init__(self, *args, **kargs):
         Logger.__init__(self, *args, **kargs)
         discord_notifier = DiscordNotifier('DISCORD_WEBHOOK_URL')
         notifier_handler = NotifierHandler(discord_notifier)
         notifier_handler.setLevel(ERROR)
+        
         self.logger.propagate = False
         self.logger.addHandler(notifier_handler)
